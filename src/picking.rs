1e73b1f863ed505a3c3feecd84338134c37950f4
WaterFace <kevinvisserweckworth@gmail.com>
2023-10-12 05:41:02 -0400

    use egui to set the cursor icon instead of bevy so we don't conflict with egui setting the cursor

--- src/picking.rs
+++ src/picking.rs
@@ -1,28 +1,39 @@
 use bevy::{prelude::*, window::PrimaryWindow};
+use bevy_egui::EguiContexts;
 use bevy_mod_picking::prelude::*;
 
 #[derive(Default)]
-struct CursorStack(Vec<CursorIcon>);
+struct CursorStack(Vec<bevy_egui::egui::CursorIcon>);
 
 fn handle_pointer_state(
+    mut contexts: EguiContexts,
     mut over_reader: EventReader<Pointer<Over>>,
     mut out_reader: EventReader<Pointer<Out>>,
-    mut main_window_query: Query<&mut Window, With<PrimaryWindow>>,
+    main_window_query: Query<Entity, With<PrimaryWindow>>,
     mut stack: Local<CursorStack>,
 ) {
-    let Ok(mut main_window) = main_window_query.get_single_mut() else {
+    let Ok(window) = main_window_query.get_single() else {
+        return;
+    };
+    let Some(ctx) = contexts.try_ctx_for_window_mut(window) else {
         return;
     };
 
     for _ev in over_reader.into_iter() {
-        stack.0.push(CursorIcon::Hand);
+        stack.0.push(bevy_egui::egui::CursorIcon::PointingHand);
     }
 
     for _ev in out_reader.into_iter() {
         let _ = stack.0.pop();
     }
 
-    main_window.cursor.icon = *stack.0.last().unwrap_or(&CursorIcon::Default);
+    // main_window.cursor.icon = *stack.0.last().unwrap_or(&CursorIcon::Default);
+    ctx.set_cursor_icon(
+        *stack
+            .0
+            .last()
+            .unwrap_or(&bevy_egui::egui::CursorIcon::Default),
+    );
 }
 
 pub struct PickingPlugin;
